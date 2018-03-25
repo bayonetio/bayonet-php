7290b0c87d84df2e6a0bd47ba28fa3eea9606afd
imran-bayonet <imran@bayonet.io>
2018-03-25 17:44:51 -0600

    Add init config to load dependencies when client is not using composer

---
+++ init.php
@@ -0,0 +1,10 @@
+<?php
+
+// Bayonet base
+require(dirname(__FILE__) . '/src/BayonetClient.php');
+// Api helper
+require(dirname(__FILE__) . '/src/ApiHelper.php');
+// Clients
+require(dirname(__FILE__) . '/src/DeviceFingerprintClient.php');
+require(dirname(__FILE__) . '/src/EcommerceClient.php');
+require(dirname(__FILE__) . '/src/LendingClient.php');
