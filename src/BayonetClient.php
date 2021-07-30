724bafc850374874e530cea1f463f77be447cf21
ilugobayo <ivan.lugo@bayonet.io>
2021-07-30 06:20:17 -0600

    Updates src files
    Removes the use of guzzle, now the SDK uses cURL.
    Version of the API is now hardcoded.
    Removes get fingeprint data endpoint.
    Updates remaining endpoints and adds blocklist/whitelist endpoints.

--- src/BayonetClient.php
+++ src/BayonetClient.php
@@ -2,31 +2,23 @@
 
 namespace Bayonet;
 
-class BayonetClient {
-
+class BayonetClient
+{
     protected $client;
     protected $config;
-    protected $supported_versions = [2];
 
-    public function __construct(array $config = []) {
-        if(!isset($config['api_key'])) {
+    public function __construct(array $config = [])
+    {
+        if (!isset($config['api_key'])) {
             $config['api_key'] = getenv('BAYONET_API_KEY');
         }
 
-        if(!isset($config['version'])) {
-            throw new \Exception('You need to specify a Bayonet API version');
-        }
-        else if(!in_array($config['version'], $this->supported_versions)) {
-          throw new \Exception('The specified version is not supported');
-        }
-
         $this->config = $config;
-        $this->config['base_uri'] = 'https://api.bayonet.io/v' . $this->config['version'] . '/';
-        $this->config['base_uri_fingerprinting'] = 'https://fingerprinting.bayonet.io/v1/';
+        $this->config['base_uri'] = 'https://api.bayonet.io/v2/';
     }
 
-    public function getConfig() {
+    public function getConfig()
+    {
         return $this->config;
     }
 }
-?>
